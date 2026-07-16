a91c63df542fea6a443dc6c9ba0d921c46aebed0
chaploud - Shota Kudo <in-the-cloud.950@hotmail.co.jp>
2026-07-16 09:57:55 +0900

    cljw 1.3.1

--- Formula/cljw.rb
+++ Formula/cljw.rb
@@ -1,20 +1,20 @@
 class Cljw < Formula
   desc "JVM-free Clojure runtime in Zig, with a WebAssembly FFI"
   homepage "https://github.com/clojurewasm/ClojureWasm"
-  version "1.3.0"
+  version "1.3.1"
   license "EPL-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/clojurewasm/ClojureWasm/releases/download/v1.3.0/cljw-macos-aarch64.tar.gz"
-      sha256 "4806a7cdc7414bd23906c6b9616056b5c1d8eb6aac5484fc0088b2584138116b"
+      url "https://github.com/clojurewasm/ClojureWasm/releases/download/v1.3.1/cljw-macos-aarch64.tar.gz"
+      sha256 "7519e6dc25ca3b3c184ca02267b060b5c3d799370edb7232b643fa37aeeaf053"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/clojurewasm/ClojureWasm/releases/download/v1.3.0/cljw-linux-x86_64.tar.gz"
-      sha256 "8a7a6497a9a976665287732f94a8fa884e2284a5780083178f9e6d791e38bbaf"
+      url "https://github.com/clojurewasm/ClojureWasm/releases/download/v1.3.1/cljw-linux-x86_64.tar.gz"
+      sha256 "dff7305d3577a31572cddbf4af56c81701ab62288d900d02c8d4f9c2ce083e78"
     end
   end
 
